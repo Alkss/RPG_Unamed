602120d45cd3df23396ae54f1c7ac723d27025d3
Alkss <alkss_fjv@yahoo.com.br>
2018-04-17 15:44:47 -0300

    muitas mudanças... perdi a noção do que fiz

--- view/user/Table/index.php
+++ view/user/Table/index.php
@@ -8,4 +8,29 @@
 require('../../../config.php');
 include('../../../header.php');
 
-header('Location:../index.php');
+if ($_SESSION['logado'] != 1) {
+    header('location:' . URL . '/view/index.php');
+} else {
+    
+    $db = new DataBase();
+    $tableInfo = $db->search("SELECT * FROM tb_usuario JOIN ta_perfil_sala ON idt_usuario=cod_usuario JOIN tb_sala ON idt_sala=cod_sala WHERE cod_sala='" . $_GET['idt'] . "'");
+    
+    if ($tableInfo[0]['cod_usuario'] != $_SESSION['idt_usuario']) {
+        $db->executeQuery("INSERT INTO `ta_perfil_sala`(`cod_usuario`, `cod_personagem`, `cod_papel_sala`, `cod_sala`)
+VALUES ('" . $_SESSION['idt_usuario'] . "',
+        '',
+        '" . '3' . "',
+        '" . $_GET['idt'] . "'");
+    }
+    ?>
+    <body id="table-home">
+    <h1><?= RPG_NAME ?></h1>
+
+    <h2>Mesa de <?=$tableInfo[0]['nme_usuario']?></h2>
+    <div class="col-xs-4">
+        <?php include 'menu.php';?>
+    </div>
+
+    </body>
+    <?php
+}
---
+++ view/user/Table/menu.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 17/01/18
+ * Time: 16:06
+ */
+?>
+<body id="menu-ADM">
+<div class="col-xs-4" id="menu-adm">
+    <p class="menuTitle">Menu</p>
+    <p><a href="<?= URL ?>view/adm/homepageADM.php" id="homeBtn">Início</a></p>
+    <hr>
+    <p>Personagens</p>
+    <?php
+    if ($tableInfo[0]['cod_papel_sala'] == '1') {
+        ?>
+        <p><a href="<?= URL ?>view/user/Character/createNewCharacter.php">Criar personagem</a></p>
+        <?php
+    }
+    ?>
+    <hr>
+    <p><a>Livro de Regras</a></p>
+    <hr>
+    <p><a>Usuários</a></p>
+    <hr>
+    <p><a>Customizáveis</a></p>
+    <p id="exit_btn"><a href="<?= URL ?>view/adm/exit.php">Sair</a></p>
+</div>
+</body>
