c12e3e76be3834e6f114b6b2596875538baa3033
Alkss <alkss_fjv@yahoo.com.br>
2018-04-17 12:24:22 -0300

    reposicionamento do botão "criar mesa"

--- view/user/index.php
+++ view/user/index.php
@@ -27,21 +27,26 @@ if ($_SESSION['logado'] != 1) {
     }
     ?>
     <div class="container">
-
+        
+        <h3>Personagens</h3>
         <div id="recent-chars">
             <iframe src="Table/showRecentChars.php" id="tableIframe"></iframe>
         </div>
+        
+        <h3>Mesas Recentes</h3>
         <div id="recent-tables">
             <iframe src="Table/showRecentTables.php" id="tableIframe"></iframe>
         </div>
 
-        <div id="menu">
-            <a class="btn btn-primary pull-right" href="Table/createTable.php">Criar nova mesa</a>
-        </div>
+        
         
         <h3>Minhas Mesas</h3>
         <div id="show-tables">
             <iframe src="Table/showTables.php" id="tableIframe"></iframe>
+        </div>
+
+        <div id="menu">
+            <a class="btn btn-primary pull-right" href="Table/createTable.php">Criar nova mesa</a>
         </div>
     </div>
     </body>
