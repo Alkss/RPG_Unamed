c9ac95e5b780227c381ec04c7ebeb17cf8598555
Alkss <alkss_fjv@yahoo.com.br>
2018-04-26 20:28:20 -0300

    fix na criação política de criação de usuário

--- view/user/Table/menu.php
+++ view/user/Table/menu.php
@@ -6,9 +6,10 @@
  * Time: 16:06
  */
 $db = new DataBase();
-$numberOfCharSQL = "SELECT * FROM tb_personagem JOIN ta_perfil_sala ON idt_personagem=cod_personagem JOIN tb_usuario ON idt_usuario=cod_usuario WHERE idt_usuario =" . $_SESSION['idt_usuario'];
+$numberOfCharSQL = "SELECT * FROM tb_personagem JOIN ta_perfil_sala ON idt_personagem=cod_personagem JOIN tb_usuario ON idt_usuario=cod_usuario WHERE idt_usuario =" . $_SESSION['idt_usuario']." AND idt_perfil_sala=". $_GET['idt'];
 $numberOfCharSQL = $db->search($numberOfCharSQL);
 $chars = $db->search("SELECT * FROM ta_perfil_sala JOIN tb_sala ON cod_sala=idt_sala JOIN tb_personagem ON cod_personagem = idt_personagem WHERE cod_sala='" . $db->scapeCont($_GET['idt']) . "'");
+
 ?>
 <div class="col-xs-2">
 
@@ -24,7 +25,8 @@ $chars = $db->search("SELECT * FROM ta_perfil_sala JOIN tb_sala ON cod_sala=idt_
             foreach ($chars as $char) {
                 ?>
                 <p>
-                    <a href="../Character/index.php?char=<?= $char['idt_personagem'] ?>&idt=<?= $_GET['idt'] ?>"><?= $char['nme_personagem'] ?>    (<?=$char['qtd_vida_personagem']?><i class="fa fa-heart-o" aria-hidden="true"></i>)
+                    <a href="../Character/index.php?char=<?= $char['idt_personagem'] ?>&idt=<?= $_GET['idt'] ?>"><?= $char['nme_personagem'] ?>
+                        (<?= $char['qtd_vida_personagem'] ?><i class="fa fa-heart-o" aria-hidden="true"></i>)
                     </a>
                 </p>
                 <?php
@@ -44,7 +46,8 @@ $chars = $db->search("SELECT * FROM ta_perfil_sala JOIN tb_sala ON cod_sala=idt_
         }
         ?>
         <hr>
-        <p><a href="http://bunkernerd.com.br/dungeons-dragons-dd-regras-basicas/" target="_blank">Livro de Regras</a></p>
+        <p><a href="http://bunkernerd.com.br/dungeons-dragons-dd-regras-basicas/" target="_blank">Livro de Regras</a>
+        </p>
         <hr>
         <p><a>Usuários</a></p>
         <hr>
