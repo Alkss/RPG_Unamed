66d1140aaf6fd9a12b4e6ca3406920a24e9727b6
Alkss <alkss_fjv@yahoo.com.br>
2018-04-26 14:56:03 -0300

    edição de personagem, concluído. Falta só o post

--- view/user/Character/editCharAux.php
+++ view/user/Character/editCharAux.php
@@ -7,102 +7,123 @@
  */
 ?>
 <div class="col-xs-10">
+    <form method="post">
+        <div class="row">
+            <div class="col-xs-3 col-xs-offset-1">
+                <?php
+                $imgURL = $selectedChar[0]['img_personagem'];
+                echo '<img id="charImg" src="' . $imgURL . '">';
+                ?>
+            </div>
 
-    <div class="row">
-        <div class="col-xs-3 col-xs-offset-1">
-            <?php
-            $imgURL = $selectedChar[0]['img_personagem'];
-            echo '<img id="charImg" src="' . $imgURL . '">';
-            ?>
+            <div class="col-xs-3">
+                <label for="editName">Nome</label>
+                <input type="text" value="<?= $selectedChar[0]['nme_personagem'] ?>" id="editName" name="editName"
+                       class="form-control">
+
+                <label for="editRace">Raça</label>
+                <select class="form-control" id="editRace" name="editRace">
+                    <?php
+                    foreach ($selectedRace as $singleRace) {
+                        if ($singleRace['idt_raca'] == $selectedChar[0]['cod_raca']) {
+                            ?>
+                            <option value="<?= $singleRace['idt_raca'] ?>"
+                                    selected="selected"><?= $singleRace['nme_raca'] ?></option>
+                            <?php
+                        } else {
+                            ?>
+                            <option value="<?= $singleRace['idt_raca'] ?>"><?= $singleRace['nme_raca'] ?></option>
+                            <?php
+                        }
+                    }
+                    ?>
+                </select>
+
+                <label for="editAlignment">Alinhamento</label>
+                <select class="form-control" id="editAlignment" name="editAlignment">
+                    <?php
+                    foreach ($selectedAlignment as $singleAlignment) {
+                        if ($singleAlignment['idt_alinhamento'] == $selectedChar[0]['cod_alinhamento']) {
+                            ?>
+                            <option value="<?= $singleAlignment['idt_alinhamento'] ?>"
+                                    selected="selected"><?= $singleAlignment['nme_alinhamento'] ?></option>
+                            <?php
+                        } else {
+                            ?>
+                            <option value="<?= $singleAlignment['idt_alinhamento'] ?>"><?= $singleAlignment['nme_alinhamento'] ?></option>
+                            <?php
+                        }
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="col-xs-2">
+                <label for="editClass">Classe</label>
+                <select class="form-control" id="editClass" name="editClass">
+                    <?php
+                    foreach ($selectedClass as $singleClass) {
+                        if ($singleClass['idt_classe'] == $selectedClass[0]['cod_classe']) {
+                            ?>
+                            <option value="<?= $singleClass['idt_classe'] ?>"
+                                    selected="selected"><?= $singleClass['nme_classe'] ?></option>
+                            <?php
+                        } else {
+                            ?>
+                            <option value="<?= $singleClass['idt_classe'] ?>"><?= $singleClass['nme_classe'] ?></option>
+                            <?php
+                        }
+                    }
+                    ?>
+                </select>
+
+                <label for="editGenre">Genêro</label>
+                <select id="editGenre" name="editGenre" class="form-control">
+                    <?php
+                    $genres = array('M' => 'Masculino', 'F' => 'Feminino');
+                    foreach ($genres as $key => $genre) {
+                        if ($selectedChar[0]['gen_personagem'] == $key) {
+                            ?>
+                            <option selected="selected" value="<?= $key ?>"><?= $genre ?></option>
+                            <?php
+                        } else {
+                            ?>
+                            <option value="<?= $key ?>"><?= $genre ?></option>
+                            <?php
+                        }
+                    }
+                    ?>
+                </select>
+
+                <label for="eyeColor">Cor dos olhos</label>
+                <input type="text" class="form-control" id="eyeColor" name="eyeColor"
+                       value="<?= $selectedChar[0]['cor_olho_personagem'] ?>">
+            </div>
+            <div class="col-xs-3">
+                <label for="height">Altura</label>
+                <input class="form-control" id="height" name="height" type="number" min="0" max="20" step="0.01"
+                       value="<?= $selectedChar[0]['alt_personagem'] ?>">
+
+                <label for="weight">Peso</label>
+                <input class="form-control" id="weight" name="weight" type="number"
+                       value="<?= $selectedChar[0]['pes_personagem'] ?>">
+
+                <br>
+                <button class="btn btn-primary" id="updateChar" name="updateChar">Atualizar informações</button>
+            </div>
         </div>
 
-        <div class="col-xs-3">
-            <label for="editName">Nome</label>
-            <input type="text" value="<?= $selectedChar[0]['nme_personagem'] ?>" id="editName" name="editName"
-                   class="form-control">
-
-            <label for="editRace">Raça</label>
-            <select class="form-control" id="editRace" name="editRace">
-                <?php
-                foreach ($selectedRace as $singleRace) {
-                    if ($singleRace['idt_raca'] == $selectedChar[0]['cod_raca']) {
-                        ?>
-                        <option value="<?= $singleRace['idt_raca'] ?>"
-                                selected="selected"><?= $singleRace['nme_raca'] ?></option>
-                        <?php
-                    } else {
-                        ?>
-                        <option value="<?= $singleRace['idt_raca'] ?>"><?= $singleRace['nme_raca'] ?></option>
-                        <?php
-                    }
-                }
-                ?>
-            </select>
-
-            <label for="editAlignment">Alinhamento</label>
-            <select class="form-control" id="editAlignment" name="editAlignment">
-                <?php
-                foreach ($selectedAlignment as $singleAlignment) {
-                    if ($singleAlignment['idt_alinhamento'] == $selectedChar[0]['cod_alinhamento']) {
-                        ?>
-                        <option value="<?= $singleAlignment['idt_alinhamento'] ?>"
-                                selected="selected"><?= $singleAlignment['nme_alinhamento'] ?></option>
-                        <?php
-                    } else {
-                        ?>
-                        <option value="<?= $singleAlignment['idt_alinhamento'] ?>"><?= $singleAlignment['nme_alinhamento'] ?></option>
-                        <?php
-                    }
-                }
-                ?>
-            </select>
+        <div class="row">
+            <div class="col-xs-3 col-xs-offset-1">
+                <label for="editImg">Link da imagem (URL)</label>
+                <input type="text" name="editImg" value="<?= $imgURL ?>" id="editImg" class="form-control">
+            </div>
         </div>
-        <div class="col-xs-2">
-            <label for="editClass">Classe</label>
-            <select class="form-control" id="editClass" name="editClass">
-                <?php
-                foreach ($selectedClass as $singleClass) {
-                    if ($singleClass['idt_classe'] == $selectedClass[0]['cod_classe']) {
-                        ?>
-                        <option value="<?= $singleClass['idt_classe'] ?>"
-                                selected="selected"><?= $singleClass['nme_classe'] ?></option>
-                        <?php
-                    } else {
-                        ?>
-                        <option value="<?= $singleClass['idt_classe'] ?>"><?= $singleClass['nme_classe'] ?></option>
-                        <?php
-                    }
-                }
-                ?>
-            </select>
-
-            <label for="editGenre">Genêro</label>
-            <select id="editGenre" name="editGenre" class="form-control">
-                <?php
-                $genres = array('M' => 'Masculino', 'F' => 'Feminino');
-                foreach ($genres as $key => $genre) {
-                    if ($selectedChar[0]['gen_personagem'] == $key) {
-                        ?>
-                        <option selected="selected" value="<?= $key ?>"><?= $genre ?></option>
-                        <?php
-                    } else {
-                        ?>
-                        <option value="<?= $key ?>"><?= $genre ?></option>
-                        <?php
-                    }
-                }
-                ?>
-            </select>
-
-            <label for="eyeColor">Cor dos olhos</label>
-            <input type="text" class="form-control" id="eyeColor" name="eyeColor" value="<?=$selectedChar[0]['cor_olho_personagem']?>">
+        <div class="row">
+            <div class="col-xs-12">
+                <h6>História</h6>
+                <textarea id="editHist" name="editHist" style="height: 180px"
+                          class="form-control"><?= $selectedChar[0]['hst_personagem'] ?></textarea>
+            </div>
         </div>
-    </div>
-
-    <div class="row">
-        <div class="col-xs-3 col-xs-offset-1">
-            <label for="editImg">Link da imagem (URL)</label>
-            <input type="text" name="editImg" value="<?= $imgURL ?>" id="editImg" class="form-control">
-        </div>
-    </div>
+    </form>
 </div>
