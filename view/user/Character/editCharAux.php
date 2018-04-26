67713e69719e55e5c95ffa1ab20ebb5165787604
Alkss <alkss_fjv@yahoo.com.br>
2018-04-26 19:15:26 -0300

    layout da página de edição

--- view/user/Character/editCharAux.php
+++ view/user/Character/editCharAux.php
@@ -100,7 +100,7 @@
                 <input required="required" type="text" class="form-control" id="eyeColor" name="eyeColor"
                        value="<?= $selectedChar[0]['cor_olho_personagem'] ?>">
             </div>
-            <div class="col-xs-3">
+            <div class="col-xs-2">
                 <label for="height">Altura</label>
                 <input required="required" class="form-control" id="height" name="height" type="number" min="0" max="20" step="0.01"
                        value="<?= $selectedChar[0]['alt_personagem'] ?>">
@@ -121,7 +121,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-xs-12">
+            <div class="col-xs-10 col-xs-offset-1">
                 <h6>História</h6>
                 <textarea id="editHist" name="editHist" style="margin-bottom:15px;text-align: justify; height: 180px"
                           class="form-control"><?= $selectedChar[0]['hst_personagem'] ?></textarea>
